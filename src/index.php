a7a599b6d6172b8f0a40e47deceb3e33251fec4b
leva1189 <levchenkodm13@gmail.com>
2020-11-15 12:14:02 +0200

    laba 2.1 -f 2020-11-15

--- src/index.php
+++ src/index.php
@@ -1,2 +1,8 @@
 <?php
+/*
+    echo "Hello". " ". "world". " ". "!". "\n";
+    echo strftime('%d-%B-%Y, %A '). "\n";
+    print strftime('%d-%B-%Y, %A '). " ". "Yes";
+    require_once ("info.php");
+*/
 require_once ("index.html");
