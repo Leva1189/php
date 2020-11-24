8b6242652152cb19cc796af433d20259d9f938b3
leva1189 <levchenkodm13@gmail.com>
2020-11-24 16:32:01 +0200

    laba 2.3 -f 2020-11-24

--- src/index.php
+++ src/index.php
@@ -4,5 +4,17 @@
     echo strftime('%d-%B-%Y, %A '). "\n";
     print strftime('%d-%B-%Y, %A '). " ". "Yes";
     require_once ("info.php");
+
+
+$name = "John  Вася";
+
+$len1 = strlen($name); // 5 (подсчет в байтах)
+$len2 = mb_strlen($name); // подсчет по символу
+//$pos = $len - 1;
+echo "$len1 $len2";
+//echo $name{$pos};
+// $go = $len{ strlen($name)-1};
+// echo $go;
 */
+
 require_once ("index.html");
