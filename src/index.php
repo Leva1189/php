00013f9f761e95976f9e39e516d3a355ccad9974
Levchenko Dmitry <levchenkodm13@gmail.com>
2021-01-27 16:16:29 +0200

    27.01.2021_OOP_lab3.1

--- src/index.php
+++ src/index.php
@@ -1,10 +1,36 @@
 <?
 
-class Pet{
+class User{
+    public $name;
+    public $login;
+    public $password;
 
+    function showInfo(){
+        echo "<hr>Name: {$this->name}<br>";
+        echo "Login: {$this->login}<br>";
+        echo "Password: {$this->password}<br>";
+    }
 }
 
-$obj1 = new Pet();
+$user1 = new User();
 
 
-//echo gettype($obj1);
+
+$user1->name='Иван';
+$user1->login='Ivan';
+$user1->password='Ivan123';
+$user1->showInfo();
+
+
+$user2 = new User();
+$user2->name='Петр';
+$user2->login='Petr';
+$user2->password='Petr123';
+$user2->showInfo();
+
+
+$user3 = new User();
+$user3->name='Василий';
+$user3->login='Vasia';
+$user3->password = "Vasia123";
+$user3->showInfo();
