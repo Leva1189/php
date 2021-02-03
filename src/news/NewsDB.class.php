7d3c754ee9e3ab0e6a9a528fc1a9df263221c3fd
Levchenko Dmitry <levchenkodm13@gmail.com>
2021-02-03 17:42:44 +0200

    OOP_SQLite3_lab3.2.3

--- src/news/NewsDB.class.php
+++ src/news/NewsDB.class.php
@@ -1,6 +1,9 @@
 <?php
 require_once "INewsDB.class.php";
 
+/**
+ * @method lastErrorMsg()
+ */
 class NewsDB implements INewsDB{
     const DB_NAME = "news.db";
     private $_db;
@@ -35,12 +38,42 @@ class NewsDB implements INewsDB{
                    ";
             $this->_db->exec($sql) or die($this->lastErrorMsg());
         }
+
+
+
+
+
+
     }
     function __destruct(){
         unset($this->_db);
     }
-    function saveNews($title, $category, $description, $source){}
+    function saveNews($title, $category, $description, $source){
+        $dt = time();
+        $sql= "INSERT INTO msgs(
+                            title, 
+                            category, 
+                            description, 
+                            source, 
+                            datetime)
+                    VALUES(
+                            '$title', 
+                            $category, 
+                            '$description', 
+                            '$source', 
+                            $dt)";
+       return $this->_db->exec($sql);
+
+    }
     function getNews(){}
     function deleteNews($id){}
+    function clearStr($data){
+        $data = strip_tags($data);
+        return $this->_db->escapeString($data);
+    }
+    function clearInt($data){
+        return abs((int)$data);
+    }
+
 }
 //$news = new NewsDB();
