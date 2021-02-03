4c39d02c41a674682b8ef1951f3f018177db5e49
Levchenko Dmitry <levchenkodm13@gmail.com>
2021-02-03 16:16:29 +0200

    OOP_SQLite3_lab3.2.2

--- src/news/NewsDB.class.php
+++ src/news/NewsDB.class.php
@@ -12,6 +12,29 @@ class NewsDB implements INewsDB{
     }
     function __construct(){
         $this->_db = new SQLite3(self::DB_NAME);
+
+        if(filesize(self::DB_NAME) == 0){
+            $sql = "CREATE TABLE msgs(
+	                id INTEGER PRIMARY KEY AUTOINCREMENT,
+	                title TEXT,
+	                category INTEGER,
+	                description TEXT,
+	                source TEXT,
+	                datetime INTEGER
+                    )";
+            $this->_db->exec($sql) or die($this->lastErrorMsg());
+            $sql = "CREATE TABLE category(
+	                id INTEGER,
+	                name TEXT
+                    )";
+            $this->_db->exec($sql) or die($this->lastErrorMsg());
+            $sql = "INSERT INTO category(id, name)
+                    SELECT 1 as id, 'Политика' as name
+                    UNION SELECT 2 as id, 'Культура' as name
+                    UNION SELECT 3 as id, 'Спорт' as name 
+                   ";
+            $this->_db->exec($sql) or die($this->lastErrorMsg());
+        }
     }
     function __destruct(){
         unset($this->_db);
@@ -20,4 +43,4 @@ class NewsDB implements INewsDB{
     function getNews(){}
     function deleteNews($id){}
 }
-$news = new NewsDB();
+//$news = new NewsDB();
